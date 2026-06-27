ad90239f206f9fce9159f3faca07c8eabdb954ba
liaac <liaac.zhang@gmail.com>
2026-06-27 09:45:18 +0800

    update skeleton

---
+++ src/array/633_平方数之和.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=633 lang=rust
+ *
+ * [633] 平方数之和
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn judge_square_sum(c: i32) -> bool {
+        true
+    }
+}
+// @lc code=end
+
