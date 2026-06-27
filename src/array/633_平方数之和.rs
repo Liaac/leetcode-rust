1127b1f937907ee725f565d912c1996ce8313192
liaac <liaac.zhang@gmail.com>
2026-06-27 09:57:50 +0800

    6.27 lc633

--- src/array/633_平方数之和.rs
+++ src/array/633_平方数之和.rs
@@ -3,11 +3,25 @@
  *
  * [633] 平方数之和
  */
+struct Solution;
 
 // @lc code=start
 impl Solution {
     pub fn judge_square_sum(c: i32) -> bool {
-        true
+        let c = c as i64;
+        let mut a = 0;
+        let mut b = (c as f64).sqrt() as i64;
+        while a <= b {
+            let sum = a * a + b * b;
+            if sum == c {
+                return true;
+            } else if sum < c {
+                a += 1;
+            } else {
+                b -= 1;
+            }
+        }
+        false
     }
 }
 // @lc code=end
